85ff80a8229dd2a1ff84b4377d0ffb17c21e5a50
Marsel <148235259+mendbv@users.noreply.github.com>
2025-03-18 20:30:09 +0600

    fixed models

--- finance/models.py
+++ finance/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='categories')
 
 
     def __str__(self):
